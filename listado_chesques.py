bf7c8dd1aa04292fe3b5716b6abe2fe61e263ae7
Guido <guido.bassi@hotmail.com>
2022-06-28 18:28:18 -0300

    Agregando las fechas

--- listado_chesques.py
+++ listado_chesques.py
@@ -3,9 +3,12 @@ nombre_archivo = "videojuegos.csv"
 with open("listado.csv", "r") as archivo:
     lector = csv.reader(archivo, delimiter=";")
     next(lector, None)
-    documento = int(input("Ingrese su documento"))
+    documento = int(input("Ingrese su documento\n"))
     for fila in lector:
         DNI = int(fila[8])
         estado = fila[9]
+        FechaOrigen = fila[6]
+        FechaPago = fila[7]
         if documento == DNI:
-            print(f"El DNI: '{DNI}' Tiene su cheque en estado: '{estado}'")
+            print(
+                f"El DNI: '{DNI}' Tiene su cheque en estado: '{estado}', la fecha de origen fue {FechaOrigen} y la fecha del pago {FechaPago}")
