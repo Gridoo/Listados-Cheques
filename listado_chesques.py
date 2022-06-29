5586a06365ef7ebb7e389d6882ef5193756b1195
agusrosa <agusrosa>
2022-06-29 13:08:53 -0300

    base

--- listado_chesques.py
+++ listado_chesques.py
@@ -1,14 +1,39 @@
 import csv
-nombre_archivo = "videojuegos.csv"
-with open("listado.csv", "r") as archivo:
-    lector = csv.reader(archivo, delimiter=";")
-    next(lector, None)
-    documento = int(input("Ingrese su documento\n"))
-    for fila in lector:
-        DNI = int(fila[8])
-        estado = fila[9]
-        FechaOrigen = fila[6]
-        FechaPago = fila[7]
-        if documento == DNI:
-            print(
-                f"El DNI: '{DNI}' Tiene su cheque en estado: '{estado}', la fecha de origen fue {FechaOrigen} y la fecha del pago {FechaPago}")
+from datetime import datetime
+
+
+dt= datetime.now()
+ts=datetime.timestamp(dt)
+print(dt, ts)
+with open("listado.csv") as archivo:
+    lector = csv.reader(archivo, delimiter=",")
+    type(lector)
+    lista=[]
+    lista=list(lector)
+    #print(lista)
+    documento=input("Ingrese su DNI: ")
+    for fila in lista:
+        NroCheque=fila[0]
+        CodigoBanco=fila[1]
+        CodigoSucursal=fila[2]
+        NumeroCuentaOrigen=fila[3]
+        NumeroCuentaDestino=fila[4]
+        Valor=fila[5]
+        FechaOrigen=fila[6]
+        FechaPago=fila[7]
+        DNI=fila[8]
+        Estado=fila[9]
+        Tipo=fila[10]
+        int(NroCheque)
+        int(CodigoBanco)
+        int(CodigoSucursal)
+        int(NumeroCuentaOrigen)
+        int(NumeroCuentaDestino)
+        float(Valor)
+        
+
+        if DNI==documento:
+            print("El dni se encuentra en la base de datos")
+            print(DNI)
+
+#NroCheque,CodigoBanco,CodigoSucursal,NumeroCuentaOrigen,NumeroCuentaDestino,Valor,FechaOrigen,FechaPago,DNI,Estado, Tipo
