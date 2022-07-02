6c74a5b6e1c1efbab8015c9c764334528980aef0
Guido <guido.bassi@hotmail.com>
2022-07-02 15:41:31 -0300

    muestra por pantalla una sola vez todas las cuentas de ese dni

--- listado_chesques.py
+++ listado_chesques.py
@@ -1,30 +1,28 @@
 import csv
-
 from datetime import datetime
 
-
-dt= datetime.now()
-ts=datetime.timestamp(dt)
+dt = datetime.now()
+ts = datetime.timestamp(dt)
 #print(dt, ts)
 with open("listado.csv") as archivo:
-    lector = csv.reader(archivo, delimiter=",")
+    lector = csv.reader(archivo, delimiter=";")
     type(lector)
-    lista=[]
-    lista=list(lector)
-    NroCheque=[]
-    CodigoBanco=[]
-    CodigoSucursal=[]
-    NumeroCuentaOrigen=[]
-    NumeroCuentaDestino=[]
-    Valor=[]
-    FechaOrigen=[]
-    FechaPago=[]
-    dni=[]
-    Estado=[]
-    Tipo=[]
-    listaCheques=[]
-    listaCuenta=[]
-    documento=input("Ingrese su DNI: ") #Ingreso cuenta origen
+    lista = []
+    lista = list(lector)
+    NroCheque = []
+    CodigoBanco = []
+    CodigoSucursal = []
+    NumeroCuentaOrigen = []
+    NumeroCuentaDestino = []
+    Valor = []
+    FechaOrigen = []
+    FechaPago = []
+    dni = []
+    Estado = []
+    Tipo = []
+    listaCheques = []
+    listaCuenta = []
+    documento = input("Ingrese su DNI: ")  # Ingreso cuenta origen
     for fila in lista:
         NroCheque.append(fila[0])
         CodigoBanco.append(fila[1])
@@ -37,24 +35,24 @@ with open("listado.csv") as archivo:
         dni.append(fila[8])
         Estado.append(fila[9])
         Tipo.append(fila[10])
-        if documento in dni:    #ver que no se repita
-            CuentasDNI=fila[3]
+        if documento in dni:  # ver que no se repita
+            CuentasDNI = fila[3]
             listaCuenta.append(CuentasDNI)
-            print("La cuenta se encuentra en la base de datos")
-            print(listaCuenta) #hasta acá va bien
-            ChequesDNI=fila[0]
+            # hasta acá va bien
+            ChequesDNI = fila[0]
             listaCheques.append(ChequesDNI)
-    for i in listaCuenta:   #ver que no se repita
-        if len (listaCheques)==len(set(listaCheques)):
+    print("La cuentas", listaCuenta, "del documento",
+          documento, "fueron encontradas en la base de datos")
+    for i in listaCuenta:  # ver que no se repita
+        if len(listaCheques) == len(set(listaCheques)):
             print("No hay repetidos")
         else:
             print("ERROR, hay cheques repetidos")
     #    if ChequesDNI in listaCheques:
 
-      #      print(listaCheques)
+    #      print(listaCheques)
 
 #NroCheque,CodigoBanco,CodigoSucursal,NumeroCuentaOrigen,NumeroCuentaDestino,Valor,FechaOrigen,FechaPago,DNI,Estado, Tipo
 
-#ingresa el dni, buscar el numero de cuenta de todos los que coinciden el dni
+# ingresa el dni, buscar el numero de cuenta de todos los que coinciden el dni
 # fijarse si un numero de cuenta tiene mas de un cheque con mismo numero
-
