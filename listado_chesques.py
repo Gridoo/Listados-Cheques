8f4befe85a0ff1facbb6d5b93b1d8eb461551b8c
agusrosa <agusrosa>
2022-07-01 22:22:59 -0300

    terminar comparacion entre cheques

--- listado_chesques.py
+++ listado_chesques.py
@@ -4,36 +4,52 @@ from datetime import datetime
 
 dt= datetime.now()
 ts=datetime.timestamp(dt)
-print(dt, ts)
+#print(dt, ts)
 with open("listado.csv") as archivo:
     lector = csv.reader(archivo, delimiter=",")
     type(lector)
     lista=[]
     lista=list(lector)
-    #print(lista)
-    documento=input("Ingrese su DNI: ")
+    NroCheque=[]
+    CodigoBanco=[]
+    CodigoSucursal=[]
+    NumeroCuentaOrigen=[]
+    NumeroCuentaDestino=[]
+    Valor=[]
+    FechaOrigen=[]
+    FechaPago=[]
+    dni=[]
+    Estado=[]
+    Tipo=[]
+    listaCheques=[]
+    listaCuenta=[]
+    documento=input("Ingrese su DNI: ") #Ingreso cuenta origen
     for fila in lista:
-        NroCheque=fila[0]
-        CodigoBanco=fila[1]
-        CodigoSucursal=fila[2]
-        NumeroCuentaOrigen=fila[3]
-        NumeroCuentaDestino=fila[4]
-        Valor=fila[5]
-        FechaOrigen=fila[6]
-        FechaPago=fila[7]
-        DNI=fila[8]
-        Estado=fila[9]
-        Tipo=fila[10]
-        int(NroCheque)
-        int(CodigoBanco)
-        int(CodigoSucursal)
-        int(NumeroCuentaOrigen)
-        int(NumeroCuentaDestino)
-        float(Valor)
-        
+        NroCheque.append(fila[0])
+        CodigoBanco.append(fila[1])
+        CodigoSucursal.append(fila[2])
+        NumeroCuentaOrigen.append(fila[3])
+        NumeroCuentaDestino.append(fila[4])
+        Valor.append(fila[5])
+        FechaOrigen.append(fila[6])
+        FechaPago.append(fila[7])
+        dni.append(fila[8])
+        Estado.append(fila[9])
+        Tipo.append(fila[10])
+        if documento in dni:
+            CuentasDNI=fila[3]
+            listaCuenta.append(CuentasDNI)
+            print("La cuenta se encuentra en la base de datos")
+            print(listaCuenta) #hasta acá va bien
+            ChequesDNI=fila[0]
+            listaCheques.append(ChequesDNI)
+        if ChequesDNI in listaCheques:
+            print(listaCheques)
+            print("Error, el DNI ingresado tiene una cuenta con más de un cheque con igual numero")
 
-        if DNI==documento:
-            print("El dni se encuentra en la base de datos")
-            print(DNI)
+
 
 #NroCheque,CodigoBanco,CodigoSucursal,NumeroCuentaOrigen,NumeroCuentaDestino,Valor,FechaOrigen,FechaPago,DNI,Estado, Tipo
+
+#ingresa el dni, buscar el numero de cuenta de todos los que coinciden el dni
+# fijarse si un numero de cuenta tiene mas de un cheque con mismo numero
