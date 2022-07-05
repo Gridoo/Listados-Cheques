de14a296587bf7683e966e0a7ff5505313c3e5b6
Guido <guido.bassi@hotmail.com>
2022-07-04 22:33:33 -0300

    Falta pulir detalles salida csv

--- listado_cheques.py
+++ listado_cheques.py
@@ -1,33 +1,39 @@
 import csv
 from datetime import datetime
 
-archivo=input("Ingrese el nombre del archivo: ")
-dni=input("Ingrese el DNI: ")
-salida=input("Ingrese el tipo de salida (pantalla/csv): ")
-tipoCheque=input("Ingrese el tipo de cheque a buscar: ")
-resultado=[]
-salida=salida.upper()
-tipoCheque=tipoCheque.upper()
+
+archivo = input("Ingrese el nombre del archivo: ")
+dni = input("Ingrese el DNI: ")
+salida = input("Ingrese el tipo de salida (pantalla/csv): ")
+tipoCheque = input("Ingrese el tipo de cheque a buscar: ")
+resultado = []
+salida = salida.upper()
+tipoCheque = tipoCheque.upper()
 with open(archivo) as file:
     lector = csv.reader(file, delimiter=";")
     for fila in lector:
-        documento=fila[8]
-        tipoChequeOriginal=fila[9]
-        estadoChequeOriginal=fila[10]
-        if documento!=dni or tipoCheque != tipoChequeOriginal:
+        documento = fila[8]
+        tipoChequeOriginal = fila[9]
+        estadoChequeOriginal = fila[10]
+        if documento != dni or tipoCheque != tipoChequeOriginal:
             continue
         resultado.append(fila)
-cantCheques=set()
+cantCheques = set()
 for fila in resultado:
-    documento=fila[8]
-    Cheques=fila[0]
-    CuentaOrigen=fila[3]
+    documento = fila[8]
+    Cheques = fila[0]
+    CuentaOrigen = fila[3]
     if (Cheques, CuentaOrigen, documento) in cantCheques:
         resultado.append("Se repite el numero de cheque más de una vez")
     else:
-        cantCheques.add((Cheques,CuentaOrigen,documento))
-if salida=="PANTALLA":
+        cantCheques.add((Cheques, CuentaOrigen, documento))
+if salida == "PANTALLA":
     for fila in resultado:
-        print("Los datos del dni ingresado son: ", documento,"\n ",fila)
-#elif salida=="CSV":
-
+        print("Los datos del dni ingresado son: ", documento, "\n ", fila)
+elif salida == "CSV":
+    filtrado = [fila[3], fila[5], fila[6], fila[7]]
+    dt = datetime.now()
+    dt = dt.strftime("%d-%m-%Y")
+    with open(f'{fila[8]}-{dt}.csv', 'w', newline='') as salida:
+        writer = csv.writer(salida)
+        writer.writerows(filtrado)
